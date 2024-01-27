702065a33352cf5ff7501ed4e2ce407514b92e42
Dremaro <dremaro29@gmail.com>
2024-01-27 13:10:36 +0100

    Creation of initial py files and dwnlded the TP2 of datascience

---
+++ TP_2_Questions.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Segment-the-nucleis" data-toc-modified-id="Segment-the-nucleis-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Segment the nucleis</a></span></li><li><span><a href="#Segment-cells" data-toc-modified-id="Segment-cells-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Segment cells</a></span></li></ul></div>
+
+# **Aims** Apply the concepts and algorithms seen to segment the cells from he screen of interest, to obtain masks with each individual cell outlined
+
+# <img src="orig.png" width="600" />
+# <img src="segmented_lab.png" width="600" />
+# <img src="segmented.png" width="600" />
+# 
+
+# In[32]:
+
+
+import re
+import random
+import omero
+from pandas import DataFrame, merge,concat
+import numpy as np
+
+from skimage import segmentation,filters,morphology,measure,feature
+from skimage.color import label2rgb
+from scipy.ndimage.morphology import binary_fill_holes, binary_closing
+from skimage.util import img_as_ubyte,img_as_float
+from skimage.exposure import rescale_intensity
+
+from scipy.stats import kurtosis, skew
+
+import omero
+from omero.gateway import BlitzGateway
+
+import matplotlib.pyplot as plt
+
+
+# In[33]:
+
+
+# Connect to the server
+def connect(hostname, username, password):
+    conn = BlitzGateway(username, password,
+                        host=hostname, secure=True)
+    print("Connected: %s" % conn.connect())
+    conn.c.enableKeepAlive(60)
+    return conn
+
+
+# In[34]:
+
+
+def getBulkAnnotationAsDf(screenID, conn):
+    ofId = None
+    sc = conn.getObject('Screen', screenID)
+    for ann in sc.listAnnotations():
+        if isinstance(ann, omero.gateway.FileAnnotationWrapper):
+            if (ann.getFile().getName() == 'bulk_annotations'):
+                if (ann.getFile().getSize()> 1476250900): #about 140Mb?
+                    print("that's a big file...")
+                    return None
+                ofId = ann.getFile().getId()
+                break
+
+    if ofId is None:
+        return None
+
+    original_file = omero.model.OriginalFileI(ofId, False)
+
+    table = conn.c.sf.sharedResources().openTable(original_file)
+    count = 0
+    try:
+        rowCount = table.getNumberOfRows()
+
+        column_names = []
+        pattern = re.compile("Phenotype \d+$")
+        for col in table.getHeaders():
+            column_names.append(col.name)
+            if pattern.match(col.name) is not None:
+                count = count + 1
+
+        black_list = []
+        column_indices = []
+        for column_name in column_names:
+            if column_name in black_list:
+                continue
+            column_indices.append(column_names.index(column_name))
+
+        table_data = table.slice(column_indices, None)
+    finally:
+        table.close()
+
+    data = []
+    for index in range(rowCount):
+        row_values = [column.values[index] for column in table_data.columns]
+        data.append(row_values)
+
+    dfAnn = DataFrame(data)
+    dfAnn.columns = column_names
+    return dfAnn, count
+
+def getOneImage(conn,weid,pos=None):
+    we = conn.getObject('Well',weid)
+    if pos==None:
+        pos=random.choice(range(we.countWellSample()))  # one random field of that well
+        
+    im = we.getImage(pos)   
+    pix = im.getPrimaryPixels()
+    I1=pix.getPlane(0,0,0)
+    I2=pix.getPlane(0,1,0)
+    I3=pix.getPlane(0,2,0)
+    I4=pix.getPlane(0,3,0)
+    I5=pix.getPlane(0,4,0)
+    I=np.stack([I1,I2,I3,I4,I5],2)
+    return I
+
+
+# In[35]:
+
+
+host = "ws://idr.openmicroscopy.org/omero-ws"
+username = "public"
+password = "public"
+screenId = 1751 #idr0033-rohban-pathways/screenA
+
+# Connect to the server
+conn = connect(host, username, password)
+
+# Downloading the annotation file for the whole screen
+# as a panda DataFrame
+dfAnn, phenotype_count = getBulkAnnotationAsDf(screenId, conn)
+dfAnn.head()
+
+
+# In[36]:
+
+
+
+weid=dfAnn[dfAnn['Control Type']=='negative control'].sample()['Well'].values
+I=getOneImage(conn,weid)
+
+
+# In[37]:
+
+
+plt.set_cmap('gray')
+
+zoomx=[100,500]
+zoomy=[100,500]
+fig,ax=plt.subplots(1,5,figsize=(30,10))
+ax[0].imshow(I[:,:,0][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+ax[1].imshow(I[:,:,1][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+ax[2].imshow(I[:,:,2][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+ax[3].imshow(I[:,:,3][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+ax[4].imshow(I[:,:,4][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+
+for x in ax.ravel():
+    x.axis("off")
+    
+
+
+# In[38]:
+
+
+I[:,:,0]
+
+
+# ## Segment the nucleis
+# 
+# 1. Starting from the DAPI image (the first, index 0), buils a binary image with nucleis only.
+# 
+# 2. Use skimage.measure.label and skimage.measure.regionprops to extract a data frame of nucleis objects with preliminary geometric measurement. Use simple manual threshold to remove outliers objects, for example those with too small areas
+# 
+
+# In[40]:
+
+
+# Survival notes !!!!
+# this TP is to link with the python code sections in the bio583_2_slides course
+I_s = np.copy(I)
+treshold = 1000
+fig,ax=plt.subplots(1,5,figsize=(30,10))
+
+for i in range(0,5):
+    I_view = I_s[:,:,i]
+    for l in range(len(I_view)):
+        ligne = I_view[l]
+        for c in ligne:
+            el = ligne[c]
+            if el > treshold:
+                I = 5000
+            else:
+                el = 0
+    ax[i].imshow(I_s[:,:,i][zoomx[0]:zoomx[1],zoomy[0]:zoomy[1]])
+
+noyaux_I = I[:,:,0] #tableaux des noyaux
+#plt.plot c'est pour les graphs
+#plt.imshow() c'est pour les images
+
+def treshold_on_image(treshold, image):
+    M = max(max(ligne_pixel) for ligne_pixel in image)
+    return [[M if pix > treshold else 0 for pix in ligne] for ligne in image]
+    
+noyaux_I = treshold_on_image(900, noyaux_I)
+            
+            
+print("ça calcule l'image...")
+plt.imshow(noyaux_I, cmap='gray')
+plt.colorbar()
+
+#1080*1080
+
+
+# 
+# ## Segment cells
+# 
+# 3. Similarly, try to segment the cell body using the third channel (index 2)
+# 
+# 3. With nucleis as seed, use the watershed segmentation algorithm (skimage.morphology.watershed) to segment single cells. (use the 'mask' option.)
+# 
+# 4. Again use regionprops to extract objects and geometric features and clean them.
+# 
+# 3. use skimage.segmentation.clear_border to remove object touching the border (optional)
+# 
+# 4. Manually look at segmented objects for selected phenotypes to check that the segmentation is robust.
+# 
+# 
+
+# In[ ]:
+
+
+
+
