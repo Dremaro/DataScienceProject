eada60886e7f3c6832cc8eaf4c806c95120059e4
Dremaro <dremaro29@gmail.com>
2024-02-13 16:40:01 +0100

    added project description

---
+++ fingerprint_main.py
@@ -0,0 +1,5 @@
+## So here we start the code for fingerprint recognition ##
+
+# 1 : We need to import some existing code or write our own to recognize the fingerprint
+# 2 : We need to evaluate how can the fingerprint be recognized when damaged or dirty
+
